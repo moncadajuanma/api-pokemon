c74a63da3663c81b1cdff8cfa1fa7bc7e0cefc13
unknown <jumamon7@gmail.com>
2024-07-21 01:56:45 -0500

    modificacion de estilos y logica

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const url = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let index = 1; index < 26; index++) {
+for (let index = 1; index <= 1021; index++) {
   fetch(url + index)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => mostrarDatos(data))
     .catch((error) => console.error("Error al obtener usuarios:", error));
 }
 
 const mostrarDatos = (data) => {
-  const div = document.createElement("div")
-  div.classList.add("list-pokemon")
+  const div = document.createElement("div");
+  div.classList.add("pokemon");
   
-};
+  let tipos = data.types.map((type) => 
+    `<p class="${type.type.name} type">${type.type.name}</p>`);
+  tipos = tipos.join("");
+
+  let id = data.id;
+  let idString = id.toString();
+
+  if (idString.length == 1) id = "00" + idString;
+  if (idString.length == 2) id = "0" + idString;
+
+  div.innerHTML = `
+    <p class="id-back">${id}</p>
+    <img src= ${data.sprites.other["official-artwork"].front_default} alt=" Pokemon ${data.name}" />
+    <div class="info-pokemon">
+      <div class="name-pokemon">
+        <p class="id">${id}</p>
+        <p class="name">${data.name}</p>
+      </div>
+      <div class="types-pokemon">
+        ${tipos}
+      </div>
+      <div class="stats-pokemon">
+        <p class="stats">Peso: ${(data.weight)/10} kg</p>
+        <p class="stats">Altura: ${(data.height)/10} m</p>
+      </div>
+    </div>`;
+    document.getElementById("lista-pokemon").append(div);
+}
