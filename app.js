6f6f81ce63f550d3dbc1229bae7fca6ee51d532c
unknown <jumamon7@gmail.com>
2024-07-25 03:09:37 -0500

    finalizacion paginacion

--- app.js
+++ app.js
@@ -1,157 +1,86 @@
 const url = "https://pokeapi.co/api/v2/pokemon/";
-const itemsPerPage = 20;
-let currentPage = 1;
-
-let btnPrevious = document.getElementById("previous");
+pokemonStart = 1;
+let pokemonPorPagina = 20;
+pokemonEnd = pokemonPorPagina;
+let btnPrevius = document.getElementById("previous");
 let btnNext = document.getElementById("next");
 
-const fetchPokemons =  (page) => {
-  let start = (page - 1) * itemsPerPage + 1;
-  let end = page * itemsPerPage;
-
-  for (let index = start; index <= end; index++) {
+const fetchpokemon = (pokemonStart, pokemonEnd) => {
+  for (let index = pokemonStart; index <= pokemonEnd; index++) {
     fetch(url + index)
       .then((response) => response.json())
       .then((data) => mostrarDatos(data))
-      .catch((error) => console.error("Error al obtener Pokémon:", error));
+      .catch((error) => console.error("Error al obtener usuarios:", error));
   }
 };
 
 const mostrarDatos = (data) => {
+  const div = document.createElement("div");
+  div.classList.add("pokemon");
 
-    const div = document.createElement("div");
-    div.classList.add("pokemon");
-    
-    let tipos = data.types.map((type) => 
-      `<p class="${type.type.name} type">${type.type.name}</p>`);
-      tipos = tipos.join("");
-  
-    let powers = data.stats.map((stat) => 
-      `<p class="stats">${stat.stat.name}: ${stat.base_stat}</p>`);
-      powers = powers.join("")
-  
-    let id = data.id;
-    let idString = id.toString();
-  
-    if (idString.length == 1) id = "00" + idString;
-    if (idString.length == 2) id = "0" + idString;
-  
-    div.innerHTML = `
-      <p class="id-back">${id}</p>
-      <img src= ${data.sprites.other["official-artwork"].front_default} alt=" Pokemon ${data.name}" />
-      <div class="info-pokemon">
-        <div class="name-pokemon">
-          <p class="id">${id}</p>
-          <p class="name">${data.name}</p>
-        </div>
-        <div class="types-pokemon">
-          ${tipos}
-        </div>
-        <div class="stats-pokemon">
-          <p class="stats">Peso: ${(data.weight)/10} kg</p>
-          <p class="stats">Altura: ${(data.height)/10} m</p>
-        </div>
-        <div class="stats-powers powers">
-          ${powers}
-        </div>
-      </div>`;
-      document.getElementById("lista-pokemon").append(div);
-  }
+  let tipos = data.types.map(
+    (type) => `<p class="${type.type.name} type">${type.type.name}</p>`
+  );
+  tipos = tipos.join("");
 
+  let powers = data.stats.map(
+    (stat) => `<p class="stats">${stat.stat.name}: ${stat.base_stat}</p>`
+  );
+  powers = powers.join("");
 
-const updatePagination = () => {
-  document.getElementById("lista-pokemon").innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
-  fetchPokemons(currentPage);
+  let id = data.id;
+  let idString = id.toString();
+
+  if (idString.length == 1) id = "00" + idString;
+  if (idString.length == 2) id = "0" + idString;
+
+  div.innerHTML = `
+    <p class="id-back">${id}</p>
+    <img src= ${
+      data.sprites.other["official-artwork"].front_default
+    } alt=" Pokemon ${data.name}" />
+    <div class="info-pokemon">
+      <div class="name-pokemon">
+        <p class="id">${id}</p>
+        <p class="name">${data.name}</p>
+      </div>
+      <div class="types-pokemon">
+        ${tipos}
+      </div>
+      <div class="stats-pokemon">
+        <p class="stats">Peso: ${data.weight / 10} kg</p>
+        <p class="stats">Altura: ${data.height / 10} m</p>
+      </div>
+      <div class="stats-powers powers">
+        ${powers}
+      </div>
+    </div>`;
+  document.getElementById("lista-pokemon").append(div);
 };
 
-btnPrevious.addEventListener("click", () => {
-  if (currentPage > 1) {
-    currentPage--;
-    updatePagination();
-  }
-});
+const previusPage = () => {
+  if (pokemonStart > 1) {
+    clear();
+    pokemonStart = pokemonStart - pokemonPorPagina;
+    pokemonEnd = pokemonEnd - pokemonPorPagina;
+    fetchpokemon(pokemonStart, pokemonEnd);
+  } else alert("No hay mas pokemon");
+};
 
-btnNext.addEventListener("click", () => {
-  currentPage++;
-  updatePagination();
-});
+const nextPage = () => {
+  if (pokemonEnd <= 1025) {
+    clear();
+    pokemonStart = pokemonStart + pokemonPorPagina;
+    pokemonEnd = pokemonEnd + pokemonPorPagina;
+    fetchpokemon(pokemonStart, pokemonEnd);
+  } else alert("Fin de la Lista");
+};
 
-// Inicializar la primera página
-updatePagination();
+const clear = () => {
+  document.getElementById("lista-pokemon").innerHTML = "";
+};
 
+fetchpokemon(pokemonStart, pokemonEnd);
 
-
-
-
-
-// const url = "https://pokeapi.co/api/v2/pokemon/";
-// pokemonStart = 1
-// pokemonEnd = 50
-
-// let btnPrevius = document.getElementById("previus");
-// let btnNext = document.getElementById("next");
-
-//   for (let index = pokemonStart; index <= pokemonEnd; index++) {
-//     fetch(url + index)
-//       .then((response) => response.json())
-//       .then((data) => mostrarDatos(data))
-//       .catch((error) => console.error("Error al obtener usuarios:", error));
-//   }
-
-// const mostrarDatos = (data) => {
-//   const div = document.createElement("div");
-//   div.classList.add("pokemon");
-  
-//   let tipos = data.types.map((type) => 
-//     `<p class="${type.type.name} type">${type.type.name}</p>`);
-//     tipos = tipos.join("");
-
-//   let powers = data.stats.map((stat) => 
-//     `<p class="stats">${stat.stat.name}: ${stat.base_stat}</p>`);
-//     powers = powers.join("")
-
-//   let id = data.id;
-//   let idString = id.toString();
-
-//   if (idString.length == 1) id = "00" + idString;
-//   if (idString.length == 2) id = "0" + idString;
-
-//   div.innerHTML = `
-//     <p class="id-back">${id}</p>
-//     <img src= ${data.sprites.other["official-artwork"].front_default} alt=" Pokemon ${data.name}" />
-//     <div class="info-pokemon">
-//       <div class="name-pokemon">
-//         <p class="id">${id}</p>
-//         <p class="name">${data.name}</p>
-//       </div>
-//       <div class="types-pokemon">
-//         ${tipos}
-//       </div>
-//       <div class="stats-pokemon">
-//         <p class="stats">Peso: ${(data.weight)/10} kg</p>
-//         <p class="stats">Altura: ${(data.height)/10} m</p>
-//       </div>
-//       <div class="stats-powers powers">
-//         ${powers}
-//       </div>
-//     </div>`;
-//     document.getElementById("lista-pokemon").append(div);
-// }
-
-// const previusPage = () => {
-//   pokemonStart -= 20;
-//   pokemonEnd -= 20;
-// }
-
-// const nextPage = () => {
-//   pokemonStart += 20;
-//   pokemonEnd += 20;
-// }
-
-// const clear = () => {
-//   div.innerHTML = "";
-// }
-
-// btnNext.addEventListener("click", nextPage);
-// btnPrevius.addEventListener("click", previusPage);
-
+btnNext.addEventListener("click", nextPage);
+btnPrevius.addEventListener("click", previusPage);
